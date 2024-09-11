daa3147e67d4ffa5af2fd3ea6e9acb6736cb0ee5
Ikeafisch3 <Ikeafisch3@gmail.com>
2024-09-11 16:58:28 +0200

    improved readability and added cache

--- ibkr/api.py
+++ ibkr/api.py
@@ -77,12 +77,15 @@ class IBKRAPI(ABC):
 ###########################################################################################################################
 
 class _conid_cache():
+    """Caches contract ids"""
+
     def __init__(self, conid_getter: Callable[[str], int]) -> None:
+        # conid_getter: function requesting conid for a symbol from ibkr api
         self._conid_getter = conid_getter
         self.cache_dir = Path(__file__).resolve().parent / "cache"
         self.cache_path = self.cache_dir / "conid.json"
         os.makedirs(self.cache_dir, exist_ok=True)
-        # Load file
+        # Load cache file
         self.conid = {}
         if os.path.exists(self.cache_path):
             with open(self.cache_path, "r") as F:
@@ -90,7 +93,7 @@ class _conid_cache():
 
     def get_conid(self, symbol: str) -> int:
         """get conid for symbol"""
-        if not symbol in self.conid:
+        if symbol not in self.conid:
             self.conid[symbol] = self._conid_getter(symbol)
             self.save()
         return self.conid.get(symbol)
@@ -106,56 +109,208 @@ class _conid_cache():
             json.dump(self.conid, F)
 
 
-class WebAPI():
-    DELAY = 1/49  # in seconds
+class _RequestCache():
+    """Caches api requests"""
+    categories = ["history"]
 
     def __init__(self) -> None:
+        self.cache_dir = Path(__file__).resolve().parent / "cache"
+        self.cache_dict_fp = self.cache_dir / "web_api_request_cache.json"
+        self.object_cache_dir = self.cache_dir / "web_api"
+        # Create folders
+        os.makedirs(self.object_cache_dir, exist_ok=True)
+        os.makedirs(self.cache_dir, exist_ok=True)
+        for category in self.categories:
+            os.makedirs(self.object_cache_dir / category, exist_ok=True)
+        os.makedirs(self.object_cache_dir / "misc", exist_ok=True)
+        # Load cache dict pointing to each filepath of each request
+        if os.path.exists(self.cache_dict_fp):
+            with open(self.cache_dict_fp, "r") as F:
+                self.cache_dict = json.load(F)
+        else:
+            self.cache_dict = {category: {} for category in self.categories}
+            self.cache_dict["misc"] = {}
+
+    def new_id(self, category: str):
+        """create a new file id"""
+        return len(os.listdir(self.object_cache_dir / category))
+
+    def read_from_cache(self, request_url: str) -> dict | list | pd.DataFrame:
+        """Gets data from cache"""
+        # from categories
+        for category in self.categories:
+            if category in request_url:
+                # make entry in category
+                return self._read_entry(category, request_url)
+        # entries without distinct category
+        return self._read_entry("misc", request_url)
+
+    def check_cache(self, request_url: str) -> bool:
+        """Checks cache for url"""
+        for category in self.categories:
+            if category in request_url:
+                return request_url in self.cache_dict[category]
+        # entries without distinct category
+        return request_url in self.cache_dict["misc"]
+
+    def _read_entry(self, category: str, request_url: str):
+        """read entry from cache_dir and load file"""
+        filename = self.cache_dict[category][request_url]
+        fp = self.object_cache_dir / category / filename
+        if str(fp).endswith("csv"):
+            return pd.read_csv(fp)
+        with open(fp, "r") as F:
+            return json.load(F)
+
+    def insert_into_cache(self, request_url: str, obj: list | dict | pd.DataFrame):
+        """Caches url and data (`obj`)"""
+        # organize into categories
+        for category in self.categories:
+            if category in request_url:
+                # make entry in category
+                self._write_entry(category, request_url, obj)
+                return
+        # entries without distinct category
+        self._write_entry("misc", request_url, obj)
+
+    def _write_entry(self, category: str, request_url: str, obj: dict | list | pd.DataFrame):
+        """save data to cache_dict and disk"""
+        # make entry in cache_dict
+        filetype = "csv" if isinstance(obj, pd.DataFrame) else "json"
+        filename = f"{self.new_id(category)}.{filetype}"
+        self.cache_dict[category][request_url] = filename
+        # save data
+        fp = self.object_cache_dir / category / filename
+        with open(fp, "w+") as F:
+            if filetype == "json":
+                json.dump(obj, F)
+            else:
+                obj.to_csv(F)
+        # save cache_dict
+        with open(self.cache_dict_fp, "w+") as F:
+            json.dump(self.cache_dict, F)
+
+
+class WebAPI():
+    DELAY = 1/9  # in seconds
+    DO_REQUEST_CACHING = True
+    """Enables caching of api requests.\n
+    **can take up a lot of disk space**"""
+
+    def __init__(self) -> None:
+        if not GATEWAY_ENABLED:
+            logging.error(
+                "Can't connect to IBKR Gateway. WebAPI is therefore inaccessible. Only cached data is available. (Sometimes wrongly triggers, eg. 503, None)")
         self.API_URL = baseUrl
-        self.security_type = "STK"  # Stonks TODO: use this attr
+        self.security_type = "STK"  # Stonks
         self.conid = _conid_cache(self._get_conid_by_symbol)
+        """Contract id cache"""
         self._last_request_timestamp = 0
         self.return_as_pandas = True
+        if self.DO_REQUEST_CACHING:
+            self.request_cache = _RequestCache()
 
     def _request(self, url: str, accept_list=False, **query_params):
-        # for better error handling
+        """Sends request to IBKR web API under pacing limitations.
+        Parses reponse to python object.
+
+        Args:
+            url (str): (partial) url to request.
+            accept_list (bool, optional): If the response is a list, do not only return the first item. Defaults to False.
+            query_params (dict): Additional parameters added to the url.
+
+        Returns:
+            Response: Python object containing the API response.
+        """
         # TODO: implement better error handling
         if self.API_URL not in url:
             url = f"{self.API_URL}/{url.strip('/')}"
         # add params
-        # TODO: sanitize keys and value to prevent errors while parsing
         for key, value in query_params.items():
             if "?" not in url:
                 url += f"?{key}={str(value).lower()}"
             else:
                 url += f"&{key}={str(value).lower()}"
         logging.debug(f"Requesting {url}")
-        # ensure delay is met
+
+        # check cache for request
+        if self.DO_REQUEST_CACHING:
+            if self.request_cache.check_cache(url):
+                logging.debug(f"Found url in cache. Loading from there.")
+                data = self.request_cache.read_from_cache(url)
+                # if the response is a list, select the first item
+                if isinstance(data, list) and (not accept_list):
+                    data = data[0]
+                return data
+            else:
+                logging.debug(f"Did not find url in cache.")
+
+        # enforce pacing limit
         current_time = time.time()
         if current_time - self._last_request_timestamp < self.DELAY:
-            time.sleep(self.DELAY - (current_time -
-                       self._last_request_timestamp))
+            _sleep_time = self.DELAY - \
+                (current_time - self._last_request_timestamp)
+            logging.debug(f"Pacing. {_sleep_time}")
+            time.sleep(_sleep_time)
+
         # send request and save timestamp
         response = requests.get(url=url, verify=False)
         self._last_request_timestamp = time.time()
+
         # TODO: handle status_code != 200
-        logging.debug(f"Received status code {response.status_code}.")
-        if response.status_code >= 400:
+        status_code = response.status_code
+        logging.debug(f"Received status code {status_code}.")
+        if status_code >= 400:
             logging.error(
-                f"Received status code {response.status_code} while requesting {url}")
-        # retrieve content
+                f"Received status code {status_code} while requesting {url}")
+
+        # retrieve content and parse to python object (eg. dict, list, ...)
         def _clean(s: str):
             return s.replace("null", "None").replace("false", "False").replace("true", "True")
         response = eval(_clean(response.content.decode()))
+
+        # check cache for request
+        if self.DO_REQUEST_CACHING and (status_code < 300):
+            logging.debug("Saving request to cache.")
+            self.request_cache.insert_into_cache(url, response)
+
+        # if the response is a list, select the first item
         if isinstance(response, list) and (not accept_list):
             response = response[0]
+
         logging.debug(f"Response:\n{response}")
         return response
 
     def _get_conid_by_symbol(self, symbol: str) -> int:
-        response = self._request(f"/iserver/secdef/search?symbol={symbol}")
+        """api request contract id"""
+        response = self._request(
+            f"/iserver/secdef/search?symbol={symbol}&secType={self.security_type}")
         return response["conid"]
 
-    def get_historical(self, symbol: str, start: datetime.datetime, duration: str = "1y", interval: str = "1h", outsideRth: bool = True):
+    @staticmethod
+    def api_response_to_pandas(data_dict: dict):
+        """Convert data dict from api response to DataFrame with descriptive naming."""
+        df = pd.DataFrame(data_dict)
+        df = df.rename(columns={
+            "t": "Time",
+            "o": "Open",
+            "c": "Close",
+            "h": "High",
+            "l": "Low",
+            "v": "Volume",
+        })
+        df = df.set_index("Time")
+        df.sort_index(inplace=True)
+        return df
+
+    def get_historical(self,
+                       symbol: str,
+                       start: datetime.datetime,
+                       duration: str = "1y",
+                       interval: str = "1h",
+                       outsideRth: bool = True,
+                       target_volumeFactor: int = 100,
+                       ):
         """Get historical data for a symbol.
 
         Args:
@@ -164,22 +319,32 @@ class WebAPI():
             duration (str, optional): Available time periods {1-30}min, {1-8}h, {1-1000}d, {1-792}w, {1-182}m, {1-15}y. Defaults to "1y"
             interval (str, optional): 1min, 2min, 3min, 5min, 10min, 15min, 30min, 1h, 2h, 3h, 4h, 8h, 1d, 1w, 1m. Defaults to "1h".
             outsideRth (bool, optional): Whether data should be returned for trades outside regular trading hours. Defaults to True.
+            target_volumeFactor (int, optional): Desired volumeFactor (factor the volume is multiplied by). Defaults to 100.
         """
-        conid = self._get_conid_by_symbol(symbol)
-        # time format: 20230821-13:30:00
+        # Get and cache contract id
+        conid = self.conid.get(symbol)
+        # time format converted to this format: 20230821-13:30:00
         start = datetime.datetime.strftime(start, "%Y%m%d-%H:%M:%S")
+        # make request
         request_url = f"{baseUrl}/iserver/marketdata/history?conid={conid}&period={duration}&bar={interval}&startTime={start}"
         response = self._request(request_url, outsideRth=outsideRth)
+        # convert to pandas dataframe
         if self.return_as_pandas:
-            # TODO: implement this
-            logging.error("Not yet implemented. Returning as dict.")
+            data = response["data"]
+            df = WebAPI.api_response_to_pandas(data)
+            # scale volume
+            volumeFactor = response["volumeFactor"]
+            if volumeFactor != target_volumeFactor:
+                df["Volume"] = df["Volume"] * \
+                    volumeFactor / target_volumeFactor
+            return df
         return response
 
 
 # tests
 if __name__ == "__main__":
     api = WebAPI()
-    api.return_as_pandas = False
+    api.return_as_pandas = True
     data = api.get_historical("GME", datetime.datetime(2023, 7, 1, 13, 30, 0))
     print(data)
 
